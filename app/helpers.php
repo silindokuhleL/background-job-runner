6f88f04ffca293522ec4d75d8d1dc10c30ae2b68
silindokuhleL <silindokuhlelmapiyeye@gmail.com>
2024-11-12 18:15:46 +0200

    added a helper function for running background jobs

---
+++ app/helpers.php
@@ -0,0 +1,20 @@
+<?php
+use App\Services\BackgroundJobRunner;
+
+if (!function_exists('runBackgroundJob')) {
+    /**
+     * Run a background job.
+     *
+     * @param string $className
+     * @param string $methodName
+     * @param array $params
+     * @param int $maxRetries
+     * @param int $delay
+     * @return void
+     */
+    function runBackgroundJob(string $className, string $methodName, array $params = [], int $maxRetries = 3, int $delay = 0): void
+    {
+        $jobRunner = new BackgroundJobRunner();
+        $jobRunner->runInBackground($className, $methodName, $params, $maxRetries, $delay);
+    }
+}
